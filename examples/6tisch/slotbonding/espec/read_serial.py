46b349152d7fccfb40c79e3a54f1b66b89540c39
Glenn Daneels <glenndaneels@gmail.com>
2020-12-15 20:00:22 +0100

    Updated read_serial.py to better function.

--- examples/6tisch/slotbonding/espec/read_serial.py
+++ examples/6tisch/slotbonding/espec/read_serial.py
@@ -6,20 +6,31 @@ import sys
 log_file = sys.argv[1]
 
 ser = serial.Serial(
-    port='/dev/ttyUSB0', \
+    port='/dev/ttyUSB1', \
     baudrate=115200, \
     parity=serial.PARITY_NONE, \
     stopbits=serial.STOPBITS_ONE, \
     bytesize=serial.EIGHTBITS, \
-    timeout=0)
+    timeout=None)
+# put timeout to None, so it blocks until readline reads a full line
+# if you put it at 0, it sometimes read only half a line? Does not happen when putting it to None.
+
 # print("connected to: " + ser.portstr)
+
+#prevline = 'test\n'
 
 with open(log_file, 'a', buffering=1) as f:
     f.write("****** Start logging experiment iteration at {0}. ******\n".format(datetime.datetime.now()))
     while True:
-        line = ser.readline();
+        line = ser.readline()
+        #line = ser.read_until(b'\r')
         #if line:
         #       print(line),i
-        if line:
-            f.write("{0} - {1}\n".format(datetime.datetime.now(), str(line)))
+        #if line:
+            #line = line.decode('ascii')
+            #if '\n' in prevline:
+        f.write("{0} - {1}\n".format(datetime.datetime.now(), line))
+            #else:
+            #f.write("{0}".format(line))
+            #prevline = line
 ser.close()
